f6e236e6169b4a4562b39ce04a1afbec97ccc0ea
Gowtham Behara <gothsiddu@gmail.com>
2019-12-27 16:04:44 +0530

    Create a test controller

---
+++ src/main/java/com/gowtham/tictactoe/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.gowtham.tictactoe.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "this is a test controller";
+    }
+}
